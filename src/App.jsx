52b0e22df99a468f9280d164ba3a7993df58ae57
MuneneCalvin <munenecalvn@gmail.com>
2023-08-26 13:17:49 +0300

    Feat: fetch users from the api

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Navbar from './Componets/Navbar/navbar';
+// import Footer from './Componets/Footer/footer';
+import noAvatar from './assets/noavatar.png';
+import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        setUsers(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  const handleSearch = () => {
+    const filteredUsers = users.filter(user =>
+      user.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setSearchResults(filteredUsers);
+  }
+
+  const handleDelete = (userId) => {
+    // Filter out the user with the given userId
+    const updatedUsers = users.filter(user => user.id !== userId);
+    setUsers(updatedUsers);
+  }
+
   return (
-    <div>App</div>
+    <main>
+      {/* Navbar */}
+      <Navbar />
+
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Search for users..."
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button className='search-btn' onClick={handleSearch}>Search</button>
+        {/* <button className='add-user-btn' onClick={handleAddUser}>Add User</button> */}
+      </div>
+
+      <div className="user-card">
+      <div className="card-container">
+        {users.map(user => (
+          <div key={user.id} className="card">
+            <img src={noAvatar} alt="noAvatar" />
+            <h2>{user.name}</h2>
+            <p>Username: {user.username}</p>
+            <p>Email: {user.email}</p>
+            <p>Phone: {user.phone}</p>
+            <p>Website: {user.website}</p>
+            <button className='delete-btn' onClick={() => handleDelete(user.id)}>Delete</button>
+          </div>
+        ))}
+      </div>
+      
+      </div>
+
+
+      {/* Footer */}
+      {/* <Footer /> */}
+    </main>
   )
 }
 
-export default App
+export default App;
---
+++ src/Componets/Navbar/navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './navbar.css'
+
+function navbar() {
+    return (
+
+        <header id="header" class="header">
+            <div class="container">
+                <nav class="nav">
+                    <a href="index.html" class="nav__brand">
+                        <i class="ri-code-s-slash-line"></i>DevelopedByCal
+                    </a>
+                    <div class="nav__menu" id="nav-menu">
+                        <ul class="nav__list">
+                            <li class="nav__item">
+                                <a href="#hero" class="nav__link">Home</a>
+                            </li>
+                            <li class="nav__item">
+                                <a href="#contact"  class="nav__link">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="nav__toggle">
+                        <i id="nav-toggle" class="ri-menu-3-line"></i>
+                    </div>
+                </nav>
+            </div>
+        </header>
+    )
+}
+
+export default navbar;
