c22c61849e9b06a3a945e4797ed47f19c67dd3e0
MuneneCalvin <munenecalvn@gmail.com>
2023-08-26 15:38:34 +0300

    feat: added new user form

--- src/Pages/addUser/addUser.jsx
+++ src/Pages/addUser/addUser.jsx
@@ -1,5 +1,6 @@
 import {useState} from 'react';
 import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
 import Navbar from '../../Components/Navbar/navbar';
 import './addUser.css';
 
@@ -8,8 +9,17 @@ function addUser() {
         name: '',
         username: '',
         email: '',
+        address: {
+            street: '',
+            city: '',
+        },
         phone: '',
-        website: ''
+        website: '',
+        company: {
+            name: '',
+            catchPhrase: '',
+            bs: '',
+        },
     });
 
     const handleSubmit = async (e) => {
@@ -17,6 +27,16 @@ function addUser() {
         try {
             const response = await axios.post('https://jsonplaceholder.typicode.com/users', userData);
             console.log('New User added:', response.data);
+            toast.success('🦄 Wow so easy!', {
+                position: "top-right",
+                autoClose: 13,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+                });
         } catch (error) {
             console.log('Error:', error);
         }
@@ -38,19 +58,26 @@ function addUser() {
 
         {/* Add User */}
         <section id="addUser" class="addUser">
-            <div class="container">
+            <div class="add-container">
                 <div class="section-title">
                     <h2>Add New User</h2>
                 </div>
                 <form onSubmit={handleSubmit}>
-                <input type="text" name="name" placeholder="Name" value={userData.name} onChange={handleInputChange} />
-                <input type="text" name="username" placeholder="Username" value={userData.username} onChange={handleInputChange} />
-                <input type="email" name="email" placeholder="Email" value={userData.email} onChange={handleInputChange} />
-                <input type="text" name="phone" placeholder="Phone" value={userData.phone} onChange={handleInputChange} />
-                <input type="text" name="website" placeholder="Website" value={userData.website} onChange={handleInputChange} />
-
-                <button type="submit" class="btn btn-primary">Add User</button>
+                    <input type="text" name="name" placeholder="Name" value={userData.name} onChange={handleInputChange} />
+                    <input type="text" name="username" placeholder="Username" value={userData.username} onChange={handleInputChange} />
+                    <input type="email" name="email" placeholder="Email" value={userData.email} onChange={handleInputChange} />
+                    <input type="text" name="street" placeholder="Street" value={userData.address.street} onChange={handleInputChange} />
+                    <input type="text" name="city" placeholder="City" value={userData.address.city} onChange={handleInputChange} />
+                    <input type="text" name="phone" placeholder="Phone" value={userData.phone} onChange={handleInputChange} />
+                    <input type="text" name="website" placeholder="Website" value={userData.website} onChange={handleInputChange} />
+                    <input type="text" name="companyName" placeholder="Company Name" value={userData.company.name} onChange={handleInputChange} />
+                    <input type="text" name="catchPhrase" placeholder="Catch Phrase" value={userData.company.catchPhrase} onChange={handleInputChange} />
+                    <input type="text" name="bs" placeholder="BS" value={userData.company.bs} onChange={handleInputChange} />
                 </form>
+
+                <div className="button">
+                <button type="submit" class="add-btn">Add User</button>
+                </div>
             </div>
         </section>
         </>
