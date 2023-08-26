95ae24efec9001f73cfcc01cfdd176558117be44
MuneneCalvin <munenecalvn@gmail.com>
2023-08-26 13:44:44 +0300

    Feat: Added search input

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Navbar from './Componets/Navbar/navbar';
-// import Footer from './Componets/Footer/footer';
 import noAvatar from './assets/noavatar.png';
 import './App.css';
 
@@ -21,56 +19,57 @@ function App() {
       });
   }, []);
 
-  const handleSearch = () => {
+  useEffect(() => {
     const filteredUsers = users.filter(user =>
       user.name.toLowerCase().includes(searchQuery.toLowerCase())
     );
     setSearchResults(filteredUsers);
-  }
+  }, [searchQuery, users]);
 
   const handleDelete = (userId) => {
-    // Filter out the user with the given userId
     const updatedUsers = users.filter(user => user.id !== userId);
     setUsers(updatedUsers);
   }
 
   return (
-    <main>
-      {/* Navbar */}
-      <Navbar />
+    <>
+      <main>
+        <Navbar />
 
-      <div className="search-bar">
-        <input
-          type="text"
-          placeholder="Search for users..."
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-        />
-        <button className='search-btn' onClick={handleSearch}>Search</button>
-        {/* <button className='add-user-btn' onClick={handleAddUser}>Add User</button> */}
-      </div>
+        <div className="top-bar">
+        <div className="search-bar">
+          <input
+            type="text"
+            placeholder="Search for users..."
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+          <button className='search-btn'>Search</button>
+        </div>
 
-      <div className="user-card">
-      <div className="card-container">
-        {users.map(user => (
-          <div key={user.id} className="card">
-            <img src={noAvatar} alt="noAvatar" />
-            <h2>{user.name}</h2>
-            <p>Username: {user.username}</p>
-            <p>Email: {user.email}</p>
-            <p>Phone: {user.phone}</p>
-            <p>Website: {user.website}</p>
-            <button className='delete-btn' onClick={() => handleDelete(user.id)}>Delete</button>
+        <div className="add-user">
+          <button className='add-user-btn'>Add User</button>
+        </div>
+        </div>
+
+        <div className="user-card">
+          <div className="card-container">
+            {searchResults.map(user => (
+              <div key={user.id} className="card">
+                <img src={noAvatar} alt="noAvatar" />
+                <h2>{user.name}</h2>
+                <p>Username: {user.username}</p>
+                <p>Email: {user.email}</p>
+                <p>Phone: {user.phone}</p>
+                <p>Website: {user.website}</p>
+                <button className='delete-btn' onClick={() => handleDelete(user.id)}>Delete</button>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-      
-      </div>
+        </div>
 
-
-      {/* Footer */}
-      {/* <Footer /> */}
-    </main>
+      </main>
+    </>
   )
 }
 
