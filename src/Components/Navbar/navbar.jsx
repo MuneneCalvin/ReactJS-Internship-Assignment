bb033584daeef137c3b7c38366899b02527786ec
MuneneCalvin <munenecalvn@gmail.com>
2023-08-28 12:56:07 +0300

    feat: responsive navbar

--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -1,19 +1,25 @@
-import React from 'react';
+import { useState} from 'react';
 import { Link } from 'react-router-dom';
 import { CiMenuFries } from 'react-icons/ci';
 import { BsCodeSlash } from 'react-icons/bs';
 import './navbar.css'
 
 function navbar() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
 
-        <header id="header" className="header">
+        <header id="header" className={`header ${isMenuOpen ? 'header--scroll' : ''}`}>
             <div class="container">
                 <nav class="nav">
                     <a href="/" className="nav__brand">
                         <BsCodeSlash className="codeslash-line"/>DevelopedByCal
                     </a>
-                    <div className="nav__menu" id="nav-menu">
+                    <div  className={`nav__menu ${isMenuOpen ? 'nav__menu--open' : ''}`} id="nav-menu">
                         <ul className="nav__list">
                             <li className="nav__item">
                                 <Link to='/' className='nav__link'>Home</Link>
@@ -23,7 +29,7 @@ function navbar() {
                             </li>
                         </ul>
                     </div>
-                    <div className="nav__toggle">
+                    <div className="nav__toggle" onClick={toggleMenu}>
                         < CiMenuFries id="nav-toggle" className="ri-menu-3-line"  />
                     </div>
                 </nav>
